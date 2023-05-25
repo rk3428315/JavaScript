6f4ecf7d5edc8f703f8edfccb1bc1d7451c532fe
Your Name <you@example.com>
2023-05-25 21:52:15 +0530

    code added

--- JavaScript/function.js
+++ JavaScript/function.js
@@ -30,3 +30,46 @@ let hello =() =>{
 hello();
 
 
+//Practice set of loop
+
+//Q 1
+
+let marks={
+    ravi:20,
+    ajeet:40,
+    anand:50,
+    prince:70
+}
+
+for(let i = 0; i< Object.keys(marks).length; i++){
+    console.log("The marks of "+ Object.keys(marks)[i] + " are "+ marks[Object.keys(marks)[i]]); //doubt
+}
+
+//same thing with for in loop
+
+for(let key in marks){
+    console.log("The marks of "+ key + " are "+ marks[key]);
+}
+
+//Q 2
+
+let number = 5 ;
+//let y;        doubt  without delclare y is possible in while loop
+// while(y != number){
+//     y =prompt("Enter a number");
+//     console.log("try again")
+// }
+// console.log("Right number");
+
+//Q 3
+
+let mean =(a, b, c, d)=>{
+    return (a +b+c+d)/4;
+}
+
+console.log (mean(4, 5, 6, 7));
+
+
+
+
+
