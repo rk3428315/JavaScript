cea4fee5976131a35a3da672cbcac9292f8f6857
Ravi Kumar <ravikumar08979@gmail.com>
2023-05-30 14:24:47 +0530

    Practice of console object method

--- Chapter5.js
+++ Chapter5.js
@@ -1,10 +1,10 @@
 //Q 1
 
 let arrrr = [1,4,6,8,54];
-let a1 = prompt("Enter a number ");
-a1= Number.parseInt(a1);
-arrrr.push(a1);
-console.log(arrrr);
+// let a1 = prompt("Enter a number ");
+// a1= Number.parseInt(a1);
+// arrrr.push(a1);
+// console.log(arrrr);
 
 //Q 2
 
@@ -25,13 +25,18 @@ console.log(filterarray);
 
 //Q 4
 
+// To get the square of number 
 let filterarray1 = arrrr.map((x)=>{
-    return x * x;
+   // return x * x;                     // to get square of number
+    return Math.sqrt(x)
 });
-console.log(filterarray1);
+let squareRoot = Math.sqrt(x); 
+//console.log(filterarray1);
+
 
 //Q 5
 
+//to get value of all number after added
 let filterarray2 = arrrr.reduce((z, y)=>{
     return z + y;
 });
