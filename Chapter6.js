4985ce79ba4e292c102329aba07b0a4dc971f31d
Ravi Kumar <ravikumar08979@gmail.com>
2023-05-31 21:08:20 +0530

    Chapter 6 Practice set added

---
+++ Chapter6.js
@@ -0,0 +1,82 @@
+//Q 1------------------------
+
+// let age = prompt("Please enter age");       // Show a popup to enter input 
+// age = Number.parseInt(age);                 // Only can enter number type value in age 
+
+// let canDrive = (age) =>{                    // this function rerturn if age is greater than or equal to 18
+//     return age >= 18 ? true: false;
+// }
+
+// if(canDrive(age)){                          //condition to check age is equal to or greater than 18 or not 
+//     alert ("You can drive.");
+// }
+// else{
+//     alert ("You can not drive.");
+// }
+
+// Q 2-----------------------------
+
+ 
+// let runAgain = true;                        // Inislize a variable to conform input value
+
+// let canDrive = (age) =>{                    // this function rerturn if age is greater than or equal to 18
+//     return age >= 18 ? true: false;
+// }
+// while(runAgain){
+
+//     let age = prompt("Please enter age");       // Show a popup to enter input 
+//     age = Number.parseInt(age);                 // Only can enter number type value in age    
+
+
+// if(canDrive(age)){                          //condition to check age is equal to or greater than 18 or not 
+//     alert ("You can drive.");
+// }
+// else{
+//     alert ("You can not drive.");
+// }
+//    runAgain = confirm("You want to give input again !")     // To conform a popup to show the input popup again
+// }
+
+// Q 3 -----------------------------
+
+// let runAgain = true;                        // Inislize a variable to conform input value
+
+// let canDrive = (age) =>{                    // this function rerturn if age is greater than or equal to 18
+//     return age >= 18 ? true: false;
+// }
+// while(runAgain){
+
+//     let age = prompt("Please enter age");       // Show a popup to enter input 
+//     age = Number.parseInt(age);                 // Only can enter number type value in age    
+
+
+// if(canDrive(age)){                          //condition to check age is equal to or greater than 18 or not 
+//     alert ("You can drive.");
+// }
+// else if(age <0){                           // Condition to don't enter negative number otherwise it will breack 
+//     console.error("Please enter a valid age");
+//     break;
+// }
+// else{
+//     alert ("You can not drive.");
+// }
+//    runAgain = confirm("You want to give input again !")     // To conform a popup to show the input popup again
+// }
+
+
+//Q 4--------------------
+
+// let number = prompt("Enter number");
+// number =Number.parseInt(number);
+
+// if(number > 4){                                        //Condition to redirecte on google website
+//     location.href = "https://google.com"
+// }
+// else{
+//     console.error("Found a error")
+// }
+
+//Q 5--------------------
+
+let color = prompt("Enter color name")          // Show a popup to enter input of color name 
+document.body.style.background = color;         // To show the background color of body 
--- Exercise.js
+++ Exercise.js
@@ -4,33 +4,33 @@ gen = Number.parseInt(x);             // To convert value into number
 let input;                            // Input value that give in the at the time of guess
 let score = 100;                      // Count variable to use guesses times
 
-while(input != gen){
-    score -= 1;
-    input = prompt("Enter a number");
+// while(input != gen){
+//     score -= 1;
+//     input = prompt("Enter a number");
 
-    // If guess number is right then print this condition
-    if(input == gen){
-        console.log("Congratulation! You won.");
-        console.log(`How many times taken to guess the right number : ${100-score}`);
+//     // If guess number is right then print this condition
+//     if(input == gen){
+//         console.log("Congratulation! You won.");
+//         console.log(`How many times taken to guess the right number : ${100-score}`);
 
-    } 
+//     } 
 
-    // If guess number is greater than random value then print this condition
-    else if (input > gen && input < 100) {
-        console.log("Your number is greater than actual number.");
+//     // If guess number is greater than random value then print this condition
+//     else if (input > gen && input < 100) {
+//         console.log("Your number is greater than actual number.");
 
-    } 
+//     } 
 
-    // If guess number is smaller than random vlaue then print this condition
-    else if (input < gen && input > 0) {
-        console.log("Your number is smaller than actual number.");
+//     // If guess number is smaller than random vlaue then print this condition
+//     else if (input < gen && input > 0) {
+//         console.log("Your number is smaller than actual number.");
 
-    } 
+//     } 
 
-    // If guess number is greater than 100 then print this condition
-    else {
-        console.log("Enter number between 1 to 100.")
+//     // If guess number is greater than 100 then print this condition
+//     else {
+//         console.log("Enter number between 1 to 100.")
         
-    }
+//     }
    
-}
+// }
