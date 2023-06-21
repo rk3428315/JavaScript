262eeca0929fdd975db5a4afd47da53f3fcea23a
Ravi Kumar <ravikumar08979@gmail.com>
2023-06-21 11:05:11 +0530

    sum of jagged array and print only string value

--- Add Zig-Jag Array/AddJaggedArray.js
+++ Add Zig-Jag Array/AddJaggedArray.js
@@ -1,88 +1,30 @@
-// Inislize a jagged array
+
+
+// Sum of Jagged array
+
+// Variable to store jagged array value
 let ar = [1, 2, [15, [23], [5, 12]], [100]];
 
-let sumOr = [];
-sumOrar= ar.concat()
-console.log(ar)
-
-// Add Jagged Array in SumOfJaggedArray variable
-// let sumOfJaggedArray = ar[0] + ar[1] + ar[2][0] + ar[2][1][0] + ar[2][2][0] + ar[2][2][1] + ar[3][0];
-
-// console.log (sumOfJaggedArray);
-let total =0;
-
-let max =[];
-
-for(i=0; i<sumOr.length; i++){
-     console.log( sumOr[i]); 
-    // for(j =0; j <ar[i].length; j++){ 
-    //      //console.log(ar[i][j]);
-
-    //     for(k =0; k<ar[i][j].length; k++){
-    //         // console.log(ar[i][j][k]);
-    //         total += ar[i][j][k];
-    //         console.log(total);
-    //     }
-    // }
-}
-
-addJaggedArray =(n) =>{
-    for(i=0; i<n.length; i++){
-        // console.log( [i]); 
-        for(j =0; j <n[i].length; j++){ 
-             //console.log(ar[i][j]);
-    
-            for(k =0; k<n[i][j].length; k++){
-                // console.log(ar[i][j][k]);
-                total += n[i][j][k];
-                console.log(total);
-            }
-        }
-    }
-}
-addJaggedArray([1, 2, [15, [23], [5, 12]], [100]])
-
+// Function to return sum Of given jagged array
+function sumJaggedArray(ar)
 {
-
-    let sstring = "This is Anand  Ji";
-    let splited = sstring.split(" ")
-
-    let store = [];
-    for(i =0; i<splited.length; i++)
-        {
-            if(typeof splited[i] === 'string')
-        {
-            store.push(splited[i])
-            console.log(store);
-        }else{
-            // console.log(splited[i])
-        }
-    }    
-}
-
-{
-    let ar = [1, 2, [15, [23], [5, 12]], [100]];
-
-//console.log(sumArray(ar));
-
-function sumArray(ar)
-{
+    // To store sum value element
     let sum = 0;
-    
+    // Loop to store vlaue of ar array one by one in el variable
     for(let el of ar)
     {
+        // if el is array then iterate the value of el element by using recursivie function (sumJaggeArray(el)) and store in el 
         if (Array.isArray(el))
         {
-            el = sumArray(el);
+            el =sumJaggedArray(el)
             //console.log(el);
 
-        }
-        
+        }    
+        // sum one by one el elment  value in sum variable    
         sum += el;
-        console.log(sum);
     }
+    console.log(sum);
     
     return sum;
 }
-sumArray(ar);
-}
+sumJaggedArray(ar);
