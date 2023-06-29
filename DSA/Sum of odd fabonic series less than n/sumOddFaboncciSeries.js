e8ac46d19bcae229aebf2f993c3456c8ab99ede0
Ravi Kumar <ravikumar08979@gmail.com>
2023-06-29 11:58:20 +0530

    modified the sum of odd fabonacci series

--- DSA/Sum of odd fabonic series less than n/sumOddFaboncciSeries.js
+++ DSA/Sum of odd fabonic series less than n/sumOddFaboncciSeries.js
@@ -8,20 +8,27 @@ sumOddFabonicSeries =(n) =>{
     let sum = 0;
 
     // To print fabonacci series till the n
-    while (value <= n) {
+    while (value < n) {
+          
         // To store sum of value and i variable
         let value1 =value+i;
         // To store value of value in i
         i = value;
         // To store value of value1 in value
         value =value1
+        // conditon to check if value is greater than n then break
+        if(value > n){
+            break;
+        }  
         // To check odd fabonacci series
         if(value % 2 != 0){
+            console.log(value)
             // To sum the value of odd fabonacci series   
             sum +=value
-        }     
+        } 
+         
     }
     console.log("Sum of odd fabonacci series = ",sum)                    
-    return sum
+    return sum;
 }
-sumOddFabonicSeries(1);
+sumOddFabonicSeries(10);
