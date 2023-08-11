57ca544ee60a6c3b1a8d2a774571dea2c609d162
Ajeet <ajkumar3284@gmail.com>
2023-08-11 21:12:03 +0530

    some midified codes

--- DSA/1. Create student factory using function/createFactoryObject.js
+++ DSA/1. Create student factory using function/createFactoryObject.js
@@ -5,7 +5,23 @@ function Students(first,last,age) {
     this.lastName = last;
     this.age = age;
 }
+// This method will return the object of stu
+var crateObjectRecord = function(first, last, age){
+    // document.getElementById/
+  let data = new Students(first,last,age);
+  // append the data to html with checkbox
+  // []  First , last , age - 
+  // [] First , lastg , age 
+  
+  // selec the object by check box and click on 
+  // Compage button
 
-const d = new Students("Ajeet","kumar","22");
-const d1 = new Students("Ajeet","kumar","22");
-console.log(d,d1);
+}
+
+function compateSelectedObject(){
+    // get the selected object based on checkobx
+    // Compare the both selected object and show the result on html.
+}
+//const d = new Students("Ajeet","kumar" ,"22");
+//const d1 = new Students("Ajeet","kumar","22");
+//console.log(d,d1);
